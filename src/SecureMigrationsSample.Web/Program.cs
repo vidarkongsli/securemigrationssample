69294f5024d6d984c6f1fda4185701466cff009a
Vidar Kongsli <vidar@kongs.li>
2021-02-02 09:59:49 +0100

    Add command line parser to switch wether to run migrations and/or exit

--- src/SecureMigrationsSample.Web/Program.cs
+++ src/SecureMigrationsSample.Web/Program.cs
@@ -1,3 +1,5 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -10,19 +12,40 @@ namespace SecureMigrationsSample.Web
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            if (args.Contains("--migrate"))
-            using (var scope = host.Services.CreateScope())
+
+            var rootCommand = new RootCommand{
+                new Option<bool>("--migrate", "Whether to run database migrations"),
+                new Option<bool>("--thenExit", "Whether to exit after migrations (instead of starting web app)")
+            };
+
+            rootCommand.Handler = CommandHandler.Create<bool, bool>(async (migrate, thenExit) =>
             {
-                var sampleContext = scope.ServiceProvider.GetService<SampleContext>();
-                WriteLine($"Running migrations on db {sampleContext.Database.GetDbConnection().ConnectionString}");
-                await sampleContext.Database.MigrateAsync();
-                WriteLine("Done running migrations");
-            }
-            
-            await host.RunAsync();
+                if (migrate)
+                {
+                    using (var scope = host.Services.CreateScope())
+                    {
+                        var sampleContext = scope.ServiceProvider.GetService<SampleContext>();
+                        WriteLine($"Running migrations on db {sampleContext.Database.GetDbConnection().ConnectionString}");
+                        await sampleContext.Database.MigrateAsync();
+                        WriteLine("Done running migrations");
+                    }
+                }
+                else {
+                    WriteLine("Skipping migrations");
+                }
+                if (!thenExit)
+                {
+                    WriteLine("Starting web app.");
+                    await host.RunAsync();
+                }
+                else {
+                    WriteLine("Exit without starting web app.");
+                }
+            });
+            return await rootCommand.InvokeAsync(args);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
